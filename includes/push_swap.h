bf27ad65bb089eef650533b5dd78fdd224494832
rstephan <rstephan@student.21-school.ru>
2021-09-21 18:45:40 +0300

    base file struct

---
+++ includes/push_swap.h
@@ -0,0 +1,8 @@
+//
+// Created by Reynaldo Stephane on 9/21/21.
+//
+
+#ifndef DEV_PUSH_SWAP_H
+#define DEV_PUSH_SWAP_H
+
+#endif //DEV_PUSH_SWAP_H
---
+++ srcs/push_swap.c
@@ -0,0 +1,6 @@
+#include "../includes/push_swap.h"
+
+int main(int argc, char *argv[])
+{
+
+}
