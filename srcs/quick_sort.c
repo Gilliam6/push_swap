ad2d19645e93b55f93d290fe2412d27c88d5473d
rstephan <rstephan@student.21-school.ru>
2021-10-22 22:03:34 +0300

    check point

--- srcs/quick_sort.c
+++ srcs/quick_sort.c
@@ -24,23 +24,6 @@ int	sorted_stack(t_stack *stack)
 //	return (median);
 //}
 
-void	push_half(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
-{
-	int	len;
-
-	len = len_stack(*stack_A);
-
-	while (len--)
-	{
-		if ((*stack_A)->order <= orders->mid)
-			push_b(stack_A, stack_B);
-		else
-			shift_A(stack_A);
-	}
-	orders->max = orders->mid;
-	orders->mid = (orders->max - orders->next) / 2 + orders->next;
-	orders->group++;
-}
 
 int	check_tops(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
 {
@@ -50,6 +33,12 @@ int	check_tops(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
 		orders->next++;
 		return (1);
 	}
+//	if ((*stack_A)->next->order == orders->next)
+//	{
+//		push_b(stack_A, stack_B);
+//		shift_A(stack_A);
+//		push_a(stack_B, stack_A);
+//	}
 	if ((*stack_B) && (*stack_B)->order == orders->next)
 	{
 		(*stack_B)->group = orders->group;
@@ -58,56 +47,97 @@ int	check_tops(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
 		orders->next++;
 		return (1);
 	}
-	if ((*stack_B) && (*stack_B)->previous->order == orders->next)
-	{
-		reverse_shift_B(stack_B);
-		(*stack_B)->group = orders->group;
-		push_a(stack_B, stack_A);
-		shift_A(stack_A);
-		orders->next++;
-		return (1);
-	}
+//	if ((*stack_B) && (*stack_B)->previous->order == orders->next)
+//	{
+//		reverse_shift_B(stack_B);
+//		(*stack_B)->group = orders->group;
+//		push_a(stack_B, stack_A);
+//		shift_A(stack_A);
+//		orders->next++;
+//		return (1);
+//	}
 	return (0);
 }
 
-void	push_back(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
+void	push_half(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
 {
 	int	len;
 
 	len = len_stack(*stack_A);
 	while (len--)
 	{
-		if ((*stack_A)->order <= orders->mid)
-		{
-			(*stack_A)->group = orders->group;
+		if ((*stack_A)->order <= orders->mid && !(*stack_A)->group)
 			push_b(stack_A, stack_B);
-			if ((*stack_B)->order == orders->next)
-			{
-				orders->next++;
-				shift_B(stack_B);
-			}
-		}
 		else
 			shift_A(stack_A);
-		check_tops(stack_B, stack_A, orders);
 	}
 	orders->max = orders->mid;
 	orders->mid = (orders->max - orders->next) / 2 + orders->next;
 	orders->group++;
-//	if (*stack_B)
-//		check_tops(stack_B, stack_A, orders);
+}
+
+void 	sort_3(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
+{
+	if ((*stack_B)->next->order == orders->next)
+	{
+		swap_b(stack_B);
+		(*stack_B)->group = orders->group;
+		push_a(stack_B, stack_A);
+		shift_A(stack_A);
+	}
+	check_tops(stack_A, stack_B, orders);
+}
+
+void	push_back(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
+{
+	int	len;
+
+	len = len_stack(*stack_B);
+	while (len--)
+	{
+		if ((*stack_B)->order >= orders->mid)
+		{
+			(*stack_B)->group = orders->group;
+			push_a(stack_B, stack_A);
+			if ((*stack_A)->order == orders->next)
+			{
+				orders->next++;
+				shift_A(stack_A);
+			}
+		}
+		else
+			shift_B(stack_B);
+//		if ((*stack_B) && check_tops(stack_A, stack_B, orders))
+//			len--;
+	}
+	orders->max = orders->mid;
+	orders->mid = (orders->max - orders->next) / 2 + orders->next;
+	orders->group++;
+
 }
 
 void quick_sort(t_stack **stack_A, t_stack **stack_B, t_medians *orders)
 {
-	while (!sorted_stack(*stack_A) && !(*stack_B))
+
+	push_half(stack_A, stack_B, orders);
+	while (*stack_B)
+		push_back(stack_A, stack_B, orders);
+//	push_back(stack_A, stack_B, orders);
+	printf("orders mid %d | max %d | next %d\n", orders->mid, orders->max,
+		   orders->next);
+	while (check_tops(stack_A, stack_B, orders));
+	while((*stack_A)->group == (*stack_A)->next->group)
 	{
-		if (!(*stack_B))
-			push_half(stack_A, stack_B, orders);
-//		printf("orders mid %d | max %d | next %d\n", orders->mid, orders->max,
-//			orders->next);
-		while (*stack_B)
-			push_back(stack_B, stack_A, orders);
-		while (check_tops(stack_A, stack_B, orders));
+		check_tops(stack_A, stack_B, orders);
+		push_b(stack_A, stack_B);
 	}
+	check_tops(stack_A, stack_B, orders);
+//	push_b(stack_A, stack_B);
+//	while (*stack_B)
+//	{
+//		push_a(stack_B, stack_A);
+//		check_tops(stack_A, stack_B, orders);
+//	}
+	printf("orders mid %d | max %d | next %d\n", orders->mid, orders->max,
+		   orders->next);
 }
